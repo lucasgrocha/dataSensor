234bafb5c32437121e5102b08bc90d78b0f181bb
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-02-02 23:29:52 -0300

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
-50.times do
-  FactoryBot.create(:sensor_info, user: FactoryBot.create(:user))
+users = []
+
+25.times do
+  users << FactoryBot.create(:user)
+end
+
+150.times do
+  FactoryBot.create(:sensor_info, user: users.sample)
 end
